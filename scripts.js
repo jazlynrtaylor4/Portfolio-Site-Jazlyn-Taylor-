47068ed5911457def7c54643ffbbe82d5003b38d
jazlynrtaylor4 <jazlynrtaylor4@gmail.com>
2020-02-29 10:00:48 -0800

    2.22 changes

--- scripts.js
+++ scripts.js
@@ -1,10 +1,9 @@
-console.log("Hi there! Thanks for stopping by!")
-function menuToggle() {
-  var x = document.getElementById('myNavtoggle');
+console.log('Hi there! Thanks for stopping by!')
+function menuToggle () {
+  var x = document.getElementById('myNavtoggle')
   if (x.className === 'navtoggle') {
-    x.className += ' responsive';
+    x.className += ' responsive'
   } else {
-    x.className = 'navtoggle';
+    x.className = 'navtoggle'
   }
 }
-function onclick myFunction()
