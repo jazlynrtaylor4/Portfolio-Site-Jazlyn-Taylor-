b9714df6828b4383f736fd10ce3c03131ce8cd12
jazlynrtaylor4 <jazlynrtaylor4@gmail.com>
2020-03-01 11:03:01 -0800

    moved js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
-console.log("Hi there! Thanks for stopping by!")
+console.log('Hi there! Thanks for stopping by!')
+function menuToggle () {
+  var x = document.getElementById('myNavtoggle')
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive'
+  } else {
+    x.className = 'navtoggle'
+  }
+}
